7c1896d55d0ccd33b773b39a0c1412a1079c0aed
Pedro Ricardo <115710304+pedroricardo14@users.noreply.github.com>
2023-02-15 10:15:54 -0300

    Create funcao2.py

---
+++ funcao2.py
@@ -0,0 +1,9 @@
+def somar():
+  n1 = int(input("Primeiro número: "))
+  n2 = int(input("Segundo número: "))
+  total = n1 + n2 
+  print(f"A soma total é {total}")
+#######################################
+
+print("=== SOMADOR DE DOIS NÚMEROS ===")
+somar()
