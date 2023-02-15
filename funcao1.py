3e1b1dcc789e997bfb7dc1c7745d4b3555dce66d
Pedro Ricardo <115710304+pedroricardo14@users.noreply.github.com>
2023-02-15 10:15:03 -0300

    Create funcao1.py

---
+++ funcao1.py
@@ -0,0 +1,9 @@
+def bemvindo():
+  print("Seja bem vindo!")
+  print("É um prazer ter você aqui!")
+  print("Aproveite cada momento.")
+  ####################################
+bemvindo()
+print("Olha só que legal...")
+bemvindo()
+print("Fim.")
