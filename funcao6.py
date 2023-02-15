4db2e437b6df34f097e02e2f9596fcf307797596
Pedro Ricardo <115710304+pedroricardo14@users.noreply.github.com>
2023-02-15 10:18:55 -0300

    Create funcao6.py

---
+++ funcao6.py
@@ -0,0 +1,19 @@
+# crie uma função que retorna o dobro de um número
+def dobro(numero):
+  return numero * 2
+###################
+
+
+
+# Crie uma função que retorna o quadrado de um número 
+
+def quadrado(numero):
+  return numero * numero
+  #######################
+
+
+
+### PROGRAMA PRINCIPAL ###
+valor = int(input("Digite um valor: "))
+print("O dobro é:", dobro(valor))
+print("O quadrado é: ", quadrado(valor))
