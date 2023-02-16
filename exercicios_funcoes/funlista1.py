39869f72f0b8ecf3375a08e84d8c84d182726339
Pedro Ricardo <115710304+pedroricardo14@users.noreply.github.com>
2023-02-16 11:03:02 -0300

    Create funlista1.py

---
+++ exercicios_funcoes/funlista1.py
@@ -0,0 +1,11 @@
+#1.Crie uma função que pergunta o nome do usuário e deseja um bom dia.
+
+def bomdia():
+  nome = input("Qual é o seu nome? ")
+  print(f"Tenha um bom dia, {nome}!")
+
+#########PROGRAMA PRINCIPAL################
+
+bomdia()
+bomdia()
+bomdia()
