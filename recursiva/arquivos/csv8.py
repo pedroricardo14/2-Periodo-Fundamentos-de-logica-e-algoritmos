c319fa4d0f5cf6ea1de5b4c65a77de79a5794990
Pedro Ricardo <115710304+pedroricardo14@users.noreply.github.com>
2023-02-17 15:17:16 -0300

    Create csv8.py

---
+++ recursiva/arquivos/csv8.py
@@ -0,0 +1,15 @@
+"""arquivo = open("lista.txt","a") # w - gravação # r - leitura # a - acrescentar
+
+arquivo.write("Mosca\n")
+arquivo.write("Mosca\n")
+arquivo.write("Mosca\n")
+arquivo.close()
+print("Gravado com sucesso.")"""
+
+"""arquivo = open("lista.txt","a") # w - gravação # r - leitura # a - acrescentar
+
+arquivo.write("Borboleta\n")
+arquivo.write("Cupim\n")
+arquivo.write("Mariposa\n")
+arquivo.close()
+print("Gravado com sucesso.")"""
